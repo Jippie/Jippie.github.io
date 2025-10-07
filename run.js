9d7b251990b251f92471dea26f6220e09848d967
polewolf <22129@hetstreek.nl>
2025-10-07 09:41:02 +0200

    it do work now

--- run.js
+++ run.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const fs = require('fs');
+const path = require('path');
 
-
+const files = fs.readdirSync(__dirname);
 
 app.use(express.static(__dirname));
+
+for (let i = 0; i < files.length; i++) {
+    if (files[i].endsWith('.html')) {
+        let file = files[i].replace('.html', '');
+        app.get(`/${file}`, (req, res) => {
+            res.sendFile(path.join(__dirname, files[i]));
+        });
+    }
+}
 
 http.listen(3000, "0.0.0.0", () => {
     console.log('Server is active on http://localhost:3000');
